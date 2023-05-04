931427b292985189c4e7477f17c036fd3e0e02b1
Velheor <velheorr@gmail.com>
2023-05-04 21:36:35 +0500

    pull ready project

---
+++ src/hook/useLang.js
@@ -0,0 +1,30 @@
+import {useEffect, useState} from 'react';
+import {useDispatch} from "react-redux";
+import {updateLang} from "../store/textSlice";
+
+// получение языка браузера
+export const useLang = () => {
+	const [lang, setLang] = useState('')
+	let curLanguage = localStorage.getItem('lang')
+
+	useEffect(() => {
+		if (!curLanguage ) {
+			let language = window.navigator.language;
+			let getLanguage = language.substr(0,2)
+			localStorage.setItem('lang', getLanguage)
+			setLang(getLanguage)
+
+		}
+		console.log('localStorage("lang") =', localStorage.getItem('lang'))
+	}, [lang]);
+
+
+
+};
+
+
+
+
+
+
+
